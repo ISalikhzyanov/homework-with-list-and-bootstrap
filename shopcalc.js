d374787edef32dfb730232486535ae693ab57f56
Ilham <Ilham>
2021-12-12 14:37:18 +0300

    all name (task) changed to (buy)

--- shopcalc.js
+++ shopcalc.js
@@ -8,7 +8,7 @@ let max = 0;
 let tov = '';
 let sum = 0;
 
-class Task {
+class Buy {
     constructor(name, price) {
         this.name = name;
         this.price = price;
@@ -17,19 +17,19 @@ class Task {
 
 }
 
-class TaskList {
+class BuyList {
     constructor() {
         this.list = []
     }
 
-    add(task) {
-        this.list.push(task);
+    add(buy) {
+        this.list.push(buy);
         this.render()
     }
 
     render() {
         listEl.innerHTML = '';
-        for (const task of this.list) {
+        for (const buy of this.list) {
             const el = document.createElement('li')
             el.innerHTML = `
             <div class="alert alert-success" role="alert">
@@ -43,23 +43,23 @@ class TaskList {
     }
 }
 
-const store = new TaskList()
+const store = new BuyList()
 
 formEl.addEventListener('submit', (e) => {
     e.preventDefault()
-    const taskName = inputEl.value;
+    const buyName = inputEl.value;
     const price = Number(priceEl.value);
-    const task = new Task(taskName, Number(price));
-    store.add(task)
+    const buy = new Buy(buyName, Number(price));
+    store.add(buy)
     console.log(store.list)
-    for (const task of store.list)
-        if (task.price > max) {
-            (max = task.price) && (tov = task.name)
+    for (const buy of store.list)
+        if (buy.price > max) {
+            (max = buy.price) && (tov = buy.name)
         }
     console.log(max, tov)
     const number = store.list.length
     console.log(number)
-    for (const task of store.list)
-        sum = task.price + sum
+    for (const buy of store.list)
+        sum = buy.price + sum
   console.log(sum)
 })
